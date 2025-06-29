d59ebacd48aebc3e40a6c8cdff6b5976f481bcce
avigail <avigail.aharon9@gmail.com>
2025-06-29 12:21:51 +0300

    final version

---
+++ GenericLinkedList/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GenericLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
